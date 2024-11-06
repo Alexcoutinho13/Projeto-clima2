a4f41e2298a522e796c5e4f59571fa19772fa625
Alexcoutinhoaraujo <alex.coutinho.araujo@gmail.com>
2024-11-06 15:48:44 -0300

    at

--- js.js
+++ js.js
@@ -1,3 +1,21 @@
-document.querySelector('.busca').addEventListener('submit', (event) => {
-    event.preventDefault()
+document.querySelector('.busca').addEventListener('submit', async (event)=> {
+event.preventDefault()
+
+let input = document.querySelector('.inputSearch').value
+
+    if(input !== '') {
+        showWorning('Carregando...')
+        let url = `https://api.hgbrasil.com/weather?key=39e7ce23&city_name=${encodeURI(input)}`
+        let results = await fetch(url)
+        let json = results.json()
+        console.log(json)
+    } else {
+
+    }
+
 })
+
+
+function showWorning(msg) {
+    document.querySelector('.aviso').innerHTML = msg
+}
